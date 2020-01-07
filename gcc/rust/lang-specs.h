8a7a8bd967b5ac5b091902a0d487d12f12e62118
sapir <yasapir@gmail.com>
2020-01-07 16:29:34 +0200

    Set -fcrate-type automatically according to -shared

--- gcc/rust/lang-specs.h
+++ gcc/rust/lang-specs.h
@@ -1,2 +1,8 @@
 {".rs", "@rust", 0, 1, 0},
-{"@rust", "rust1 %i %(cc1_options) %{!fsyntax-only:%(invoke_as)}", 0, 1, 0},
+{"@rust",
+    "rust1 %i"
+    " %{shared:-fcrate-type=cdylib}"
+    " %{!shared:-fcrate-type=bin}"
+    " %(cc1_options)"
+    " %{!fsyntax-only:%(invoke_as)}",
+    0, 1, 0},
