71676de128bdf6f7b04368459f8dae40f8b3af61
sapir <yasapir@gmail.com>
2020-07-14 17:59:26 +0300

    Add a separate Type newtype for type trees

--- gcc/rust/gcc-rust/src/gcc_api.rs
+++ gcc/rust/gcc-rust/src/gcc_api.rs
@@ -44,20 +44,6 @@ impl From<TreeIndex> for Tree {
     }
 }
 
-impl From<IntegerTypeKind> for Tree {
-    fn from(itk: IntegerTypeKind) -> Self {
-        assert_ne!(itk, IntegerTypeKind::None);
-
-        Tree(unsafe { integer_types[itk as usize].0 })
-    }
-}
-
-impl From<SizeTypeKind> for Tree {
-    fn from(stk: SizeTypeKind) -> Self {
-        Tree(unsafe { sizetype_tab[stk as usize].0 })
-    }
-}
-
 impl From<BuiltinFunction> for Tree {
     fn from(bf: BuiltinFunction) -> Self {
         unsafe { _builtin_decl_implicit(bf) }
@@ -65,7 +51,7 @@ impl From<BuiltinFunction> for Tree {
 }
 
 impl Tree {
-    pub fn get_type(self) -> Self {
+    pub fn get_type(self) -> Type {
         unsafe { get_tree_type(self) }
     }
 
@@ -73,53 +59,31 @@ impl Tree {
         unsafe { get_tree_code(self) }
     }
 
-    pub fn get_type_size_bytes(self) -> Tree {
-        unsafe { get_type_size_bytes(self) }
+    fn new0(code: TreeCode, type_: Type) -> Self {
+        unsafe { Tree(build0(code, (type_.0).0)) }
     }
 
-    pub fn set_type_name(&mut self, identifier: Tree) {
-        unsafe { set_type_name(*self, identifier) }
+    pub fn new1(code: TreeCode, type_: Type, arg0: Tree) -> Self {
+        unsafe { Tree(build1(code, (type_.0).0, arg0.0)) }
     }
 
-    pub fn is_compatible_type(self, other: Tree) -> bool {
-        unsafe { useless_type_conversion_p(self.0, other.0) }
+    pub fn new2(code: TreeCode, type_: Type, arg0: Tree, arg1: Tree) -> Self {
+        unsafe { Tree(build2(code, (type_.0).0, arg0.0, arg1.0)) }
     }
 
-    pub fn new_function_type(return_type: Tree, arg_types: &[Tree]) -> Self {
-        unsafe {
-            Tree(build_function_type_array(
-                return_type.0,
-                arg_types.len() as i32,
-                arg_types.as_ptr() as *mut tree,
-            ))
-        }
-    }
-
-    pub fn new0(code: TreeCode, type_: Tree) -> Self {
-        unsafe { Tree(build0(code, type_.0)) }
-    }
-
-    pub fn new1(code: TreeCode, type_: Tree, arg0: Tree) -> Self {
-        unsafe { Tree(build1(code, type_.0, arg0.0)) }
-    }
-
-    pub fn new2(code: TreeCode, type_: Tree, arg0: Tree, arg1: Tree) -> Self {
-        unsafe { Tree(build2(code, type_.0, arg0.0, arg1.0)) }
-    }
-
-    pub fn new3(code: TreeCode, type_: Tree, arg0: Tree, arg1: Tree, arg2: Tree) -> Self {
-        unsafe { Tree(build3(code, type_.0, arg0.0, arg1.0, arg2.0)) }
+    pub fn new3(code: TreeCode, type_: Type, arg0: Tree, arg1: Tree, arg2: Tree) -> Self {
+        unsafe { Tree(build3(code, (type_.0).0, arg0.0, arg1.0, arg2.0)) }
     }
 
     pub fn new4(
         code: TreeCode,
-        type_: Tree,
+        type_: Type,
         arg0: Tree,
         arg1: Tree,
         arg2: Tree,
         arg3: Tree,
     ) -> Self {
-        unsafe { Tree(build4(code, type_.0, arg0.0, arg1.0, arg2.0, arg3.0)) }
+        unsafe { Tree(build4(code, (type_.0).0, arg0.0, arg1.0, arg2.0, arg3.0)) }
     }
 
     pub fn new_translation_unit_decl(name: Tree) -> Self {
@@ -130,12 +94,12 @@ impl Tree {
         Self::new2(TreeCode::ModifyExpr, value.get_type(), var, value)
     }
 
-    pub fn new_int_constant<T: Into<Tree>>(int_type: T, value: i64) -> Self {
-        Tree(unsafe { build_int_constant(int_type.into().0, value) })
+    pub fn new_int_constant<T: Into<Type>>(int_type: T, value: i64) -> Self {
+        Tree(unsafe { build_int_constant(int_type.into(), value) })
     }
 
     pub fn new_return_expr(value: Tree) -> Self {
-        Tree::new1(TreeCode::ReturnExpr, TreeIndex::VoidType.into(), value)
+        Tree::new1(TreeCode::ReturnExpr, Type::void(), value)
     }
 
     pub fn new_block(vars: Tree, subblocks: Tree, supercontext: Tree, chain: Tree) -> Self {
@@ -143,13 +107,7 @@ impl Tree {
     }
 
     pub fn new_bind_expr(vars: Tree, body: Tree, block: Tree) -> Self {
-        let bind_expr = Tree::new3(
-            TreeCode::BindExpr,
-            TreeIndex::VoidType.into(),
-            vars,
-            body,
-            block,
-        );
+        let bind_expr = Tree::new3(TreeCode::BindExpr, Type::void(), vars, body, block);
 
         if vars.0 != NULL_TREE.0 {
             unsafe {
@@ -160,19 +118,19 @@ impl Tree {
         bind_expr
     }
 
-    pub fn new_result_decl(loc: Location, type_: Tree) -> Self {
+    pub fn new_result_decl(loc: Location, type_: Type) -> Self {
         unsafe {
             Tree(build_decl(
                 loc.0,
                 TreeCode::ResultDecl,
                 NULL_TREE.0,
-                type_.0,
+                (type_.0).0,
             ))
         }
     }
 
-    pub fn new_var_decl(loc: Location, name: Tree, type_: Tree) -> Self {
-        let mut t = unsafe { Tree(build_decl(loc.0, TreeCode::VarDecl, name.0, type_.0)) };
+    pub fn new_var_decl(loc: Location, name: Tree, type_: Type) -> Self {
+        let mut t = unsafe { Tree(build_decl(loc.0, TreeCode::VarDecl, name.0, (type_.0).0)) };
         t.set_used(true);
         t
     }
@@ -182,11 +140,11 @@ impl Tree {
     }
 
     pub fn new_goto(label: Tree) -> Self {
-        Tree::new1(TreeCode::GotoExpr, TreeIndex::VoidType.into(), label)
+        Tree::new1(TreeCode::GotoExpr, Type::void(), label)
     }
 
     pub fn new_label_expr(decl: Tree) -> Self {
-        Tree::new1(TreeCode::LabelExpr, TreeIndex::VoidType.into(), decl)
+        Tree::new1(TreeCode::LabelExpr, Type::void(), decl)
     }
 
     pub fn new_cond_expr(cond: Tree, true_expr: Tree, false_expr: Tree) -> Self {
@@ -202,7 +160,7 @@ impl Tree {
     pub fn new_case_label_expr(value: Option<Tree>, label_decl: Tree) -> Self {
         Tree::new4(
             TreeCode::CaseLabelExpr,
-            TreeIndex::VoidType.into(),
+            Type::void(),
             value.unwrap_or(NULL_TREE),
             NULL_TREE,
             label_decl,
@@ -210,43 +168,12 @@ impl Tree {
         )
     }
 
-    pub fn new_switch_expr(switch_ty: Tree, discr: Tree, body: Tree) -> Self {
+    pub fn new_switch_expr(switch_ty: Type, discr: Tree, body: Tree) -> Self {
         Tree::new2(TreeCode::SwitchExpr, switch_ty, discr, body)
     }
 
-    pub fn new_record_type(code: TreeCode) -> Self {
-        Tree::new0(code, NULL_TREE)
-    }
-
-    pub fn place_field_manually(&mut self, byte_offset: u64) {
-        unsafe {
-            place_field_manually(*self, byte_offset);
-        }
-    }
-
-    pub fn finish_record_type(
-        &mut self,
-        mut fields: DeclList,
-        byte_size: u64,
-        byte_alignment: u64,
-    ) {
-        unsafe {
-            finish_record_type(
-                *self,
-                fields.head().unwrap_or(NULL_TREE),
-                byte_size,
-                byte_alignment,
-            );
-        }
-        fields.set_context(*self);
-    }
-
-    pub fn get_record_type_field_decl(&self, index: usize) -> Self {
-        unsafe { get_record_type_field_decl(*self, index) }
-    }
-
     pub fn new_record_constructor(
-        record_type: Tree,
+        record_type: Type,
         field_decls: &[Tree],
         field_values: &[Tree],
     ) -> Self {
@@ -261,13 +188,13 @@ impl Tree {
         }
     }
 
-    pub fn new_array_constructor(array_type: Tree, elements: &[Tree]) -> Self {
+    pub fn new_array_constructor(array_type: Type, elements: &[Tree]) -> Self {
         unsafe {
             build_constructor_from_element_array(array_type, elements.len(), elements.as_ptr())
         }
     }
 
-    pub fn new_compound_literal_expr(type_: Tree, value: Tree, context: Tree) -> Self {
+    pub fn new_compound_literal_expr(type_: Type, value: Tree, context: Tree) -> Self {
         unsafe { build_compound_literal_expr(type_, value, context) }
     }
 
@@ -287,7 +214,7 @@ impl Tree {
     }
 
     // Get the type pointed to by a pointer type tree
-    pub fn get_pointer_type_deref_type(&self) -> Self {
+    pub fn get_pointer_type_deref_type(&self) -> Type {
         assert_eq!(self.get_code(), TreeCode::PointerType);
         // Type of dereffed item is stored in POINTER_TYPE's TREE_TYPE
         self.get_type()
@@ -299,28 +226,16 @@ impl Tree {
         Self::new1(TreeCode::IndirectRef, deref_ty, base_expr)
     }
 
-    pub fn new_call_expr(loc: Location, return_type: Tree, fn_ptr: Tree, args: &[Tree]) -> Self {
+    pub fn new_call_expr(loc: Location, return_type: Type, fn_ptr: Tree, args: &[Tree]) -> Self {
         unsafe {
             Tree(build_call_array_loc(
                 loc.0,
-                return_type.0,
+                (return_type.0).0,
                 fn_ptr.0,
                 args.len() as i32,
                 args.as_ptr() as *const tree,
             ))
         }
-    }
-
-    pub fn new_signed_int_type(bits: usize) -> Self {
-        Tree(unsafe { make_signed_type(bits as i32) })
-    }
-
-    pub fn new_unsigned_int_type(bits: usize) -> Self {
-        Tree(unsafe { make_unsigned_type(bits as i32) })
-    }
-
-    pub fn new_pointer_type(to_type: Tree) -> Self {
-        Tree(unsafe { build_pointer_type(to_type.0) })
     }
 
     pub fn new_addr_expr(mut value: Tree) -> Self {
@@ -330,22 +245,14 @@ impl Tree {
 
         let mut t = Tree::new1(
             TreeCode::AddrExpr,
-            Tree::new_pointer_type(value.get_type()),
+            Type::new_pointer_type(value.get_type()),
             value,
         );
         t.set_constant(true);
         t
     }
 
-    pub fn new_vector_type(element_type: Tree, num_elements: u64) -> Self {
-        Tree(unsafe { build_vector_type(element_type.0, num_elements.try_into().unwrap()) })
-    }
-
-    pub fn new_array_type(element_type: Tree, num_elements: u64) -> Self {
-        Tree(unsafe { build_array_type_nelts(element_type.0, num_elements) })
-    }
-
-    pub fn new_array_index_ref(element_type: Tree, array_expr: Tree, index_expr: Tree) -> Self {
+    pub fn new_array_index_ref(element_type: Type, array_expr: Tree, index_expr: Tree) -> Self {
         Tree::new4(
             TreeCode::ArrayRef,
             element_type,
@@ -421,6 +328,12 @@ impl Tree {
 
         Tree(unsafe { get_identifier(name.as_ptr()) })
     }
+
+    pub fn place_field_manually(&mut self, byte_offset: u64) {
+        unsafe {
+            place_field_manually(*self, byte_offset);
+        }
+    }
 }
 
 impl std::fmt::Debug for Tree {
@@ -440,7 +353,7 @@ impl std::fmt::Debug for Tree {
             if self.get_code() == TreeCode::RecordType {
                 let mut field_decls = vec![];
                 for i in 0.. {
-                    let field_decl = self.get_record_type_field_decl(i);
+                    let field_decl = Type(*self).get_record_type_field_decl(i);
                     if field_decl.0.is_null() {
                         break;
                     }
@@ -455,6 +368,125 @@ impl std::fmt::Debug for Tree {
     }
 }
 
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug)]
+pub struct Type(Tree);
+
+impl std::ops::Deref for Type {
+    type Target = Tree;
+
+    fn deref(&self) -> &Tree {
+        &self.0
+    }
+}
+
+impl Type {
+    pub fn void() -> Self {
+        Self(TreeIndex::VoidType.into())
+    }
+
+    pub fn u16() -> Self {
+        Self(TreeIndex::Uint16Type.into())
+    }
+
+    pub fn u32() -> Self {
+        Self(TreeIndex::Uint32Type.into())
+    }
+
+    pub fn u64() -> Self {
+        Self(TreeIndex::Uint64Type.into())
+    }
+
+    pub fn bool() -> Self {
+        Self(TreeIndex::BooleanType.into())
+    }
+
+    pub fn new_function_type(return_type: Type, arg_types: &[Type]) -> Self {
+        unsafe {
+            Self(Tree(build_function_type_array(
+                (return_type.0).0,
+                arg_types.len() as i32,
+                arg_types.as_ptr() as *mut tree,
+            )))
+        }
+    }
+
+    pub fn new_record_type(code: TreeCode) -> Self {
+        Self(Tree::new0(code, Type(NULL_TREE)))
+    }
+
+    pub fn finish_record_type(
+        &mut self,
+        mut fields: DeclList,
+        byte_size: u64,
+        byte_alignment: u64,
+    ) {
+        unsafe {
+            finish_record_type(
+                *self,
+                fields.head().unwrap_or(NULL_TREE),
+                byte_size,
+                byte_alignment,
+            );
+        }
+        fields.set_context(self.0);
+    }
+
+    pub fn get_record_type_field_decl(&self, index: usize) -> Tree {
+        unsafe { get_record_type_field_decl(*self, index) }
+    }
+
+    pub fn new_signed_int_type(bits: usize) -> Self {
+        Self(Tree(unsafe { make_signed_type(bits as i32) }))
+    }
+
+    pub fn new_unsigned_int_type(bits: usize) -> Self {
+        Self(Tree(unsafe { make_unsigned_type(bits as i32) }))
+    }
+
+    pub fn new_pointer_type(to_type: Type) -> Self {
+        Self(Tree(unsafe { build_pointer_type((to_type.0).0) }))
+    }
+
+    pub fn new_vector_type(element_type: Type, num_elements: u64) -> Self {
+        Self(Tree(unsafe {
+            build_vector_type((element_type.0).0, num_elements.try_into().unwrap())
+        }))
+    }
+
+    pub fn new_array_type(element_type: Type, num_elements: u64) -> Self {
+        Self(Tree(unsafe {
+            build_array_type_nelts((element_type.0).0, num_elements)
+        }))
+    }
+
+    pub fn set_name(&mut self, identifier: Tree) {
+        unsafe { set_type_name(*self, identifier) }
+    }
+
+    pub fn get_size_bytes(self) -> Tree {
+        unsafe { get_type_size_bytes(self) }
+    }
+
+    pub fn is_compatible(self, other: Type) -> bool {
+        unsafe { useless_type_conversion_p((self.0).0, (other.0).0) }
+    }
+}
+
+impl From<IntegerTypeKind> for Type {
+    fn from(itk: IntegerTypeKind) -> Self {
+        assert_ne!(itk, IntegerTypeKind::None);
+
+        Self(Tree(unsafe { integer_types[itk as usize].0 }))
+    }
+}
+
+impl From<SizeTypeKind> for Type {
+    fn from(stk: SizeTypeKind) -> Self {
+        Self(Tree(unsafe { sizetype_tab[stk as usize].0 }))
+    }
+}
+
 extern "C" {
     static global_trees: [Tree; TreeIndex::Max as usize];
     static integer_types: [Tree; IntegerTypeKind::None as usize];
@@ -462,27 +494,27 @@ extern "C" {
 
     static crate_type: Option<NonNull<c_char>>;
 
-    fn build_int_constant(int_type: tree, value: i64) -> tree;
+    fn build_int_constant(int_type: Type, value: i64) -> tree;
 
     fn _builtin_decl_implicit(fncode: BuiltinFunction) -> Tree;
 
     fn build_constructor_from_field_array(
-        type_: Tree,
+        type_: Type,
         num_fields: usize,
         field_decls: *const Tree,
         field_values: *const Tree,
     ) -> Tree;
 
     fn build_constructor_from_element_array(
-        type_: Tree,
+        type_: Type,
         num_elements: usize,
         elements: *const Tree,
     ) -> Tree;
 
-    fn get_tree_type(tree: Tree) -> Tree;
+    fn get_tree_type(tree: Tree) -> Type;
     fn get_tree_code(tree: Tree) -> TreeCode;
-    fn get_type_size_bytes(tree: Tree) -> Tree;
-    fn set_type_name(tt: Tree, identifier: Tree);
+    fn get_type_size_bytes(tree: Type) -> Tree;
+    fn set_type_name(tt: Type, identifier: Tree);
     fn build_label_decl(loc: Location, context: Tree) -> Tree;
     fn set_tree_static(tree: Tree, value: bool);
     fn set_tree_public(tree: Tree, value: bool);
@@ -490,20 +522,20 @@ extern "C" {
     fn set_tree_constant(tree: Tree, value: bool);
     fn set_tree_used(tree: Tree, value: bool);
     fn set_tree_addressable(tree: Tree, value: bool);
-    fn make_decl_chain(code: TreeCode, num_decls: usize, types: *const Tree, decls: *mut Tree);
+    fn make_decl_chain(code: TreeCode, num_decls: usize, types: *const Type, decls: *mut Tree);
     fn set_decl_context(decl: Tree, context: Tree);
     fn set_decl_initial(decl: Tree, value: Tree);
     fn set_decl_chain_context(chain_head: Tree, context: Tree);
     fn set_decl_name(decl: Tree, identifier: Tree);
     fn place_field_manually(field_decl: Tree, byte_offset: u64);
     fn finish_record_type(
-        record_type: Tree,
+        record_type: Type,
         fields_chain_head: Tree,
         byte_size: u64,
         byte_alignment: u64,
     ) -> Tree;
-    fn get_record_type_field_decl(record_type: Tree, index: usize) -> Tree;
-    fn build_compound_literal_expr(type_: Tree, value: Tree, context: Tree) -> Tree;
+    fn get_record_type_field_decl(record_type: Type, index: usize) -> Tree;
+    fn build_compound_literal_expr(type_: Type, value: Tree, context: Tree) -> Tree;
     fn set_fn_result(fn_decl: Tree, result: Tree);
     fn set_fn_initial(fn_decl: Tree, tree: Tree);
     fn set_fn_saved_tree(fn_decl: Tree, tree: Tree);
@@ -533,7 +565,7 @@ impl StatementList {
 pub struct DeclList(Vec<Tree>);
 
 impl DeclList {
-    pub fn new(code: TreeCode, types: &[Tree]) -> Self {
+    pub fn new(code: TreeCode, types: &[Type]) -> Self {
         let mut decls = vec![NULL_TREE; types.len()];
 
         unsafe {
@@ -581,8 +613,8 @@ impl std::ops::DerefMut for DeclList {
 pub struct Function(pub Tree);
 
 impl Function {
-    pub fn new(name: &CStr, type_: Tree) -> Self {
-        Self(Tree(unsafe { build_fn_decl(name.as_ptr(), type_.0) }))
+    pub fn new(name: &CStr, type_: Type) -> Self {
+        Self(Tree(unsafe { build_fn_decl(name.as_ptr(), (type_.0).0) }))
     }
 
     pub fn set_result(&mut self, result: Tree) {
